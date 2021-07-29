3669cccd43c440ff76f712f3269f28f0170f78ca
Davy39 <davy.cottet@protonmail.com>
2021-07-29 18:32:10 +0200

    Position certif

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,9 @@ export default function Header() {
       <span className="logo icon fa-home"></span>
       <h1>{config.heading}</h1>
       <p>{config.subHeading}</p>
-      <img src={certif}  width="200" height="133" alt="" />
+      <span className="image">
+        <img src={certif} width="200" height="133" alt="" />
+      </span>
     </div>
   );
 }
