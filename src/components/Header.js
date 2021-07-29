48326dac59c6050e334ad0c2bafbf91e969cead4
Davy39 <davy.cottet@protonmail.com>
2021-07-29 13:04:06 +0200

    icone2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import config from '../../config';
 export default function Header() {
   return (
     <div id="header">
-      <span className="logo icon fa-house-day"></span>
+      <span className="logo icon fa-house-user"></span>
       <h1>{config.heading}</h1>
       <p>{config.subHeading}</p>
     </div>
