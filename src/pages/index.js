5ab0617a076848bf7980057684f63e9b9f4035a8
Davy <davy@ik.me>
2023-02-17 15:17:09 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-
+import Seo from "../components/Seo"
 import { Link } from 'gatsby';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -78,5 +78,7 @@ const IndexPage = () => (
     <Footer />
   </Layout>
 );
+
+export const Head = () => <Seo title="Ekokay" />
 
 export default IndexPage;
